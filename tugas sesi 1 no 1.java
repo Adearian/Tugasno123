98fd1d0e60e4e15739c449bd19e31b7f885c51f4
Adearian <114377164+Adearian@users.noreply.github.com>
2022-09-26 12:57:02 +0700

    Add files via upload

---
+++ tugas sesi 1 no 1.java
@@ -0,0 +1,10 @@
+
+
+public class SESI1PENGENALANKONSEPPBO {
+    public static void main(String[] args) {
+        System.out.println("Belajar JAVA");
+        System.out.println("Sangat Mudah Sekali");
+        System.out.println("dan sangat menyenangkan");
+    }
+    
+}
